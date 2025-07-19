90007dc8583c4887050a3943667f84180ead2bd6
kavya-sree-chandhi <128419692+kavya-sree-chandhi@users.noreply.github.com>
2025-07-18 21:25:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
     
     # Model Configuration
-    MODEL_NAME = os.getenv('MODEL_NAME', 'mixtral-8x7b-32768')
+    MODEL_NAME = os.getenv('MODEL_NAME', 'llama3-8b-8192')
     TEMPERATURE = float(os.getenv('TEMPERATURE', 0.7))
     MAX_TOKENS = int(os.getenv('MAX_TOKENS', 1024))
     
